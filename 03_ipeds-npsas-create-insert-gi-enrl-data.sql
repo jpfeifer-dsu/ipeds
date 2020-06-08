8a7407e13be521fc2522e3609e521cd9f1a35b41
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-06-08 10:56:58 -0600

    updated local phone and cel phone to us GTVSDAX hierarchy

--- 03_ipeds-npsas-create-insert-gi-enrl-data.sql
+++ 03_ipeds-npsas-create-insert-gi-enrl-data.sql
@@ -344,21 +344,10 @@ update enroll.ipeds_npsas_sample_20
 set citizenship_status = (select case spbpers_citz_code when '1' then 1 when '2' then 3 when '3' then 2 else -1 end
                           from spbpers
                           where spbpers_pidm = dsu_pidm);
+
 -- Student's Local Area Code and Phone Number
 update enroll.ipeds_npsas_sample_20
-set phone1_number = (select s1.sprtele_phone_area || s1.sprtele_phone_number
-                     from sprtele s1
-                     where s1.sprtele_pidm = dsu_pidm
-                       and s1.sprtele_tele_code = '01'
-                       and LENGTH(trim(translate(s1.sprtele_phone_area, ' +-.0123456789', ' '))) is null
-                       and LENGTH(s1.sprtele_phone_area) = '3'
-                       and s1.sprtele_seqno = (select MAX(s2.sprtele_seqno)
-                                               from sprtele s2
-                                               where s2.sprtele_pidm = s1.sprtele_pidm
-                                                 and s2.sprtele_tele_code = '01'
-                                                 and LENGTH(
-                                                         trim(translate(s1.sprtele_phone_area, ' +-.0123456789', ' '))) is null
-                                                 and LENGTH(s1.sprtele_phone_area) = '3'));
+set phone1_number = dsc.f_get_formatted_tele(sprtele_pidm, 'LOCPERM', 'none');
 
 update enroll.ipeds_npsas_sample_20
 set phone1_type = '1'
@@ -366,19 +355,7 @@ where phone1_number is not null;
 
 -- Student's Cell Phone Area Code and Phone Number
 update enroll.ipeds_npsas_sample_20
-set phone2_number = (select substr(s1.sprtele_phone_area || s1.sprtele_phone_number, 1, 10)
-                     from sprtele s1
-                     where s1.sprtele_pidm = dsu_pidm
-                       and s1.sprtele_tele_code = 'CELL'
-                       and LENGTH(trim(translate(s1.sprtele_phone_area, ' +-.0123456789', ' '))) is null
-                       and LENGTH(s1.sprtele_phone_area) = '3'
-                       and s1.sprtele_seqno = (select MAX(s2.sprtele_seqno)
-                                               from sprtele s2
-                                               where s2.sprtele_pidm = s1.sprtele_pidm
-                                                 and s2.sprtele_tele_code = 'CELL'
-                                                 and LENGTH(
-                                                         trim(translate(s1.sprtele_phone_area, ' +-.0123456789', ' '))) is null
-                                                 and LENGTH(s1.sprtele_phone_area) = '3'));
+set phone2_number = dsc.f_get_formatted_tele(sprtele_pidm, 'CELLTELE', 'none');
 
 update enroll.ipeds_npsas_sample_20
 set phone2_type = '2'
